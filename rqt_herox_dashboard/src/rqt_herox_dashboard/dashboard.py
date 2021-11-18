674f27237f1c59e0ec60894c241b045328c07e79
Bernhard Vorhofer <bernhard.vorhofer@tum.de>
2021-11-18 22:21:01 +0100

    Updated dashboard with UI for coordinator (missions)

--- rqt_herox_dashboard/src/rqt_herox_dashboard/dashboard.py
+++ rqt_herox_dashboard/src/rqt_herox_dashboard/dashboard.py
@@ -6,13 +6,27 @@ from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 from herox_coordinator.msg import SubsystemStatus, SubsystemStatusSingle
-from herox_coordinator.srv import SubsystemControl, SubsystemControlResponse
+from herox_coordinator.srv import SubsystemControl, SubsystemControlResponse, LoadMission, NewMission, SaveMission
 from std_srvs.srv import Trigger
 from actionlib_msgs.msg import GoalID
+from geometry_msgs.msg import Pose
 
 baseState = False
 navState = False
 teleopState = False
+
+# Values for colored terminal output
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 class HeroxDashboard(Plugin):
 
@@ -49,11 +63,34 @@ class HeroxDashboard(Plugin):
         self._cancelGoalPub = rospy.Publisher('/move_base/cancel', GoalID, queue_size=1)
 
         # Set up services
-        #rospy.wait_for_service('/driver/halt')
-        self._haltService = rospy.ServiceProxy('/driver/halt', Trigger)
-
-        print("Waiting for /subsystem_control service...")
+        print("Waiting for services...")
         rospy.wait_for_service('/subsystem_control')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /subsystem_control')
+        rospy.wait_for_service('/driver/halt')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /driver/halt')
+        rospy.wait_for_service('/load_mission')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /load_mission')
+        rospy.wait_for_service('/new_mission')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /new_mission')
+        rospy.wait_for_service('/save_mission')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /save_mission')
+        rospy.wait_for_service('/start_mission')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /start_mission')
+        rospy.wait_for_service('/stop_mission')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /stop_mission')
+        rospy.wait_for_service('/add_waypoint')
+        print(bcolors.OKGREEN + u'\u2713' + bcolors.ENDC + ' /add_waypoint')
+
+        print(bcolors.OKGREEN + 'All services online.' + bcolors.ENDC)
+
+        self._haltService = rospy.ServiceProxy('/driver/halt', Trigger)
+        self._loadService = rospy.ServiceProxy('/load_mission', LoadMission)
+        self._newService = rospy.ServiceProxy('/new_mission', NewMission)
+        self._saveService = rospy.ServiceProxy('/save_mission', SaveMission)
+        self._startService = rospy.ServiceProxy('/start_mission', Trigger)
+        self._stopService = rospy.ServiceProxy('/stop_mission', Trigger)
+        self._addWPService = rospy.ServiceProxy('/add_waypoint', Trigger)
+
         self._subsysService = rospy.ServiceProxy('/subsystem_control', SubsystemControl)
 
         # Set up button callbacks
@@ -62,6 +99,13 @@ class HeroxDashboard(Plugin):
         self._widget.btnTeleop.clicked.connect(self.btnTeleop_callback)
         self._widget.btnCancelGoal.clicked.connect(self.btnCancelGoal_callback)
         self._widget.btnEstop.clicked.connect(self.btnEstop_callback)
+        self._widget.btnLoadMission.clicked.connect(self.btnLoad_callback)
+        self._widget.btnSaveMission.clicked.connect(self.btnSave_callback)
+        self._widget.btnAddWaypoint.clicked.connect(self.btnAddWP_callback)
+        self._widget.btnStartMission.clicked.connect(self.btnStart_callback)
+        self._widget.btnStopMission.clicked.connect(self.btnStop_callback)
+        
+        self._widget.setWindowTitle('HEROX Dashboard')
 
         # Show _widget.windowTitle on left-top of each plugin (when 
         # it's set in _widget). This is useful when you open multiple 
@@ -125,6 +169,28 @@ class HeroxDashboard(Plugin):
             self._haltService()
         except rospy.ServiceException as e:
             print("Service call failed: %s"%e)
+
+    def btnLoad_callback(self):
+        mission = self._widget.cbxMission.currentText()
+        print('Requesting to load mission ' + mission)
+        self._loadService(mission)
+
+    def btnSave_callback(self):
+        mission = self._widget.cbxMission.currentText()
+        print('Requesting to save mission ' + mission)
+        self._saveService(mission)
+        
+    def btnAddWP_callback(self):
+        print('Requesting to add WP at current pose')
+        self._addWPService()
+
+    def btnStart_callback(self):
+        print('Requesting to start mission')
+        self._startService()
+
+    def btnStop_callback(self):
+        print('Requesting to stop mission')
+        self._stopService()
 
     def shutdown_plugin(self):
         # TODO unregister all publishers here
