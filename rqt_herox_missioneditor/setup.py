f35c171622db31f9316d88efc540ba42fb79ec3c
Bernhard Vorhofer <bernhard.vorhofer@robco.de>
2023-04-26 10:12:13 +0200

    Initial commit

--- rqt_herox_missioneditor/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup_args = generate_distutils_setup(
-            packages=['rqt_herox_missioneditor'],
-                package_dir={'': 'src'},
-                )
-
-setup(**setup_args)
